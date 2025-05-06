90eeffc724fa53c80df6684761683ec390dd87e0
{aksa_carvalho} <{aksacarvalho43416@gmail.com}>
2025-05-06 14:33:21 -0300

    Primeiro commit - adicionando arquivos iniciais

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import {
+  Title,
+  Container,
+  TopBackground,
+  Form,
+  ContainerInputs,
+  Input,
+  InputLabel,
+  Button,
+} from './styles'
+
+import UsersImage from './assets/users.png'
+
+function Home() {
+  return (
+    <Container>
+      <TopBackground>
+        <img src={UsersImage} alt='imagem-usuarios'/>
+      </TopBackground>
+
+      <Form>
+        <Title>Cadastro Usuário</Title>
+
+        <ContainerInputs>
+          <div>
+            <InputLabel>
+              Nome<span> *</span>
+            </InputLabel>
+            <Input type="text" placeholder="Nome do usuário" />
+          </div>
+          
+          <div>
+            <InputLabel>
+              Idade<span> *</span>
+            </InputLabel>
+            <Input type="number" placeholder="Idade do usuário" />
+          </div>
+          </ContainerInputs>
+
+
+          <div style={{width: '100%'}} >
+            <InputLabel>
+              E-mail<span> *</span>
+            </InputLabel>
+            <Input type="email" placeholder="E-mail do usuário" />
+          </div>
+        
+
+        <Button>Cadastra Usuário</Button>
+      </Form>
+    </Container>
+  );
+}
+
+export default Home;
