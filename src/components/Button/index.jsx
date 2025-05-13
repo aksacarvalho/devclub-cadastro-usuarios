d9fac4e388c81b7d13dcdf3e744a531b9181a003
{aksa_carvalho} <{aksacarvalho43416@gmail.com}>
2025-05-13 14:38:34 -0300

    add Proptypes

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,12 +1,18 @@
+import PropTypes from 'prop-types'
+
 import { Button } from './styles'
 
 function DefaultButton({ children, ...props }){
    // spread operator -> Todo o resto    
      
-     console.log(props)
+     
       return (
             <Button {...props}>{children}</Button>
       )
+}
+
+DefaultButton.propTypes = {
+    children: PropTypes.node.isRequired
 }
 
 export default DefaultButton
