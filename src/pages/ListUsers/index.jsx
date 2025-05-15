84b9ee3d5988ca24f2b8cd3d400ddc6a39f39f2e
{aksa_carvalho} <{aksacarvalho43416@gmail.com}>
2025-05-15 14:19:24 -0300

    add useEffect/useState

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,16 +1,41 @@
-import Button from '../../components/Button'
-import TopBackground from '../../components/TopBackground'
+import { useEffect, useState } from "react";
 
-function ListUsers(){
+import api from "../../services/api";
+
+import Button from "../../components/Button";
+import TopBackground from "../../components/TopBackground";
+
+function ListUsers() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    async function getUsers() {
+      const { data } = await api.get("/usuarios");
+      
+      setUsers(data)
+      
+    }
+    getUsers();
+  }, []);
 
   return (
-   <div>
-     <TopBackground />
+    <div>
+      <TopBackground />
 
-     <h1>Listagem de Usuários</h1>
-     <Button>Voltar</Button>
-   </div>
-  )
+ 
+      <h1>Listagem de Usuários</h1>
+
+      {users.map( user => (
+        <div>
+           <p>{user.name}</p>
+           <p>{user.email}</p>
+           <p>{user.age}</p>
+        </div>
+      
+      ))}
+      <Button>Voltar</Button>
+    </div>
+  );
 }
 
-export default ListUsers
+export default ListUsers;
